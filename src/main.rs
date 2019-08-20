ee820476e4aac7d4e932155c76428219de310247
Eran Cohen <ceranco@gmail.com>
2019-08-20 08:53:35 +0300

    Added GridParams to enable more sane passing of the grid parameters.

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,89 @@
 use ggez;
 use ggez::event;
-use ggez::timer;
 use ggez::graphics;
 use ggez::nalgebra as na;
+use ggez::timer;
 
-struct MainState {
-    pos_x: f32,
+struct GridParams {
+    /// The number of cells in each (row, column) of the grid.
+    size: (usize, usize),
+    /// The size of each cell (width, height) in pixels.
+    cell_size: (usize, usize),
+    /// The color with which to fill a cell (if needed).
+    cell_color: graphics::Color,
+    /// The width of the lines that mark the grid in pixels.
+    line_width: f32,
+    /// The color with which to draw the lines that mark the grid.
+    line_color: graphics::Color,
 }
 
-impl MainState {
-    fn new() -> ggez::GameResult<MainState> {
-        let s = MainState { pos_x: 0.0 };
-        Ok(s)
+impl GridParams {
+    fn generate_mesh(&self, ctx: &mut ggez::Context) -> ggez::GameResult<graphics::Mesh> {
+        let mut builder = graphics::MeshBuilder::new();
+
+        // Generate vertical lines.
+        for i in 0..=self.size.0 {
+            let x = (i * self.cell_size.0) as f32;
+
+            builder.line(
+                &[
+                    na::Point2::new(x, 0.0),
+                    na::Point2::new(x, (self.size.1 * self.cell_size.1) as f32),
+                ],
+                self.line_width,
+                self.line_color,
+            )?;
+        }
+        // Generate horizontal lines.
+        for i in 0..=self.size.1 {
+            let y = (i * self.cell_size.1) as f32;
+
+            builder.line(
+                &[
+                    na::Point2::new(0.0, y),
+                    na::Point2::new((self.size.0 * self.cell_size.0) as f32, y),
+                ],
+                self.line_width,
+                self.line_color,
+            )?;
+        }
+
+        builder.build(ctx)
     }
 }
 
-impl event::EventHandler for MainState {
+struct GameState {
+    grid_params: GridParams,
+    grid_mesh: graphics::Mesh,
+}
+
+impl GameState {
+    fn new(ctx: &mut ggez::Context) -> ggez::GameResult<GameState> {
+        let params = GridParams {
+            size: (4, 5),
+            cell_size: (64, 32),
+            cell_color: graphics::BLACK,
+            line_width: 2.0,
+            line_color: graphics::WHITE,
+        };
+        let mesh = params.generate_mesh(ctx)?;
+        let state = GameState {
+            grid_params: params,
+            grid_mesh: mesh,
+        };
+        Ok(state)
+    }
+}
+
+impl event::EventHandler for GameState {
     fn update(&mut self, _ctx: &mut ggez::Context) -> ggez::GameResult {
-        self.pos_x = self.pos_x % 800.0 + 1.0;
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult {
         graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
 
-        let circle = graphics::Mesh::new_circle(
-            ctx,
-            graphics::DrawMode::fill(),
-            na::Point2::new(self.pos_x, 380.0),
-            100.0,
-            2.0,
-            graphics::WHITE,
-        )?;
-        graphics::draw(ctx, &circle, (na::Point2::new(0.0, 0.0),))?;
+        graphics::draw(ctx, &self.grid_mesh, (na::Point2::new(0.0, 0.0),))?;
 
         // Print the fps counter to the screen.
         let fps_counter = graphics::Text::new(format!("{}", timer::fps(ctx) as i32));
@@ -43,9 +94,9 @@ impl event::EventHandler for MainState {
     }
 }
 
-pub fn main() -> ggez::GameResult { 
+pub fn main() -> ggez::GameResult {
     let cb = ggez::ContextBuilder::new("super_simple", "ggez");
     let (ctx, event_loop) = &mut cb.build()?;
-    let state = &mut MainState::new()?;
+    let state = &mut GameState::new(ctx)?;
     event::run(ctx, event_loop, state)
 }
