efba74d02cd782467022eee2a6c70560c54fe233
Eran Cohen <ceranco@gmail.com>
2019-08-20 14:45:52 +0300

    Add stub for generate_grid_cells_mesh function.

--- src/main.rs
+++ src/main.rs
@@ -17,39 +17,52 @@ struct GridParams {
     line_color: graphics::Color,
 }
 
-impl GridParams {
-    fn generate_mesh(&self, ctx: &mut ggez::Context) -> ggez::GameResult<graphics::Mesh> {
-        let mut builder = graphics::MeshBuilder::new();
+fn generate_grid_mesh(
+    ctx: &mut ggez::Context,
+    params: &GridParams,
+) -> ggez::GameResult<graphics::Mesh> {
+    let mut builder = graphics::MeshBuilder::new();
 
-        // Generate vertical lines.
-        for i in 0..=self.size.0 {
-            let x = (i * self.cell_size.0) as f32;
+    // Generate vertical lines.
+    for i in 0..=params.size.0 {
+        let x = (i * params.cell_size.0) as f32;
 
-            builder.line(
-                &[
-                    na::Point2::new(x, 0.0),
-                    na::Point2::new(x, (self.size.1 * self.cell_size.1) as f32),
-                ],
-                self.line_width,
-                self.line_color,
-            )?;
-        }
-        // Generate horizontal lines.
-        for i in 0..=self.size.1 {
-            let y = (i * self.cell_size.1) as f32;
-
-            builder.line(
-                &[
-                    na::Point2::new(0.0, y),
-                    na::Point2::new((self.size.0 * self.cell_size.0) as f32, y),
-                ],
-                self.line_width,
-                self.line_color,
-            )?;
-        }
-
-        builder.build(ctx)
+        builder.line(
+            &[
+                na::Point2::new(x, 0.0),
+                na::Point2::new(x, (params.size.1 * params.cell_size.1) as f32),
+            ],
+            params.line_width,
+            params.line_color,
+        )?;
     }
+    // Generate horizontal lines.
+    for i in 0..=params.size.1 {
+        let y = (i * params.cell_size.1) as f32;
+
+        builder.line(
+            &[
+                na::Point2::new(0.0, y),
+                na::Point2::new((params.size.0 * params.cell_size.0) as f32, y),
+            ],
+            params.line_width,
+            params.line_color,
+        )?;
+    }
+
+    builder.build(ctx)
+}
+
+fn generate_grid_cells_mesh(
+    ctx: &mut ggez::Context,
+    params: &GridParams,
+    grid_state: &Vec<Vec<bool>>,
+) -> ggez::GameResult<graphics::Mesh> {
+    let mut builder = graphics::MeshBuilder::new();
+
+    Err(ggez::GameError::RenderError(String::from("No vertices in mesh")))
+    // let mesh = builder.build(ctx)?;
+    // Ok(mesh)
 }
 
 struct GameState {
@@ -66,7 +79,7 @@ impl GameState {
             line_width: 2.0,
             line_color: graphics::WHITE,
         };
-        let mesh = params.generate_mesh(ctx)?;
+        let mesh = generate_grid_mesh(ctx, &params)?;
         let state = GameState {
             grid_params: params,
             grid_mesh: mesh,
@@ -83,7 +96,16 @@ impl event::EventHandler for GameState {
     fn draw(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult {
         graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
 
+        // draw the grid outline
         graphics::draw(ctx, &self.grid_mesh, (na::Point2::new(0.0, 0.0),))?;
+        
+        // draw the grid cells
+        let grid_cells_mesh = generate_grid_cells_mesh(ctx, &self.grid_params, &vec![vec![false]]);
+        match grid_cells_mesh {
+            Ok(mesh) => graphics::draw(ctx, &mesh, (na::Point2::new(0.0, 0.0),))?,
+            _ => (),
+        }
+        
 
         // Print the fps counter to the screen.
         let fps_counter = graphics::Text::new(format!("{}", timer::fps(ctx) as i32));
@@ -95,7 +117,11 @@ impl event::EventHandler for GameState {
 }
 
 pub fn main() -> ggez::GameResult {
-    let cb = ggez::ContextBuilder::new("super_simple", "ggez");
+    let cb = ggez::ContextBuilder::new("life-rs", "Eran Cohen").window_setup(
+        ggez::conf::WindowSetup::default()
+            .title("Game of Life")
+            .vsync(true),
+    );
     let (ctx, event_loop) = &mut cb.build()?;
     let state = &mut GameState::new(ctx)?;
     event::run(ctx, event_loop, state)
